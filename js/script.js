08c47ee241090c7f89a773e33222002dbb4c985f
Isha <isha.s@simplify3x.com>
2025-10-14 12:07:14 +0530

    removed unwanted code

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
   // Initialize all interactive elements
   initTabs();
   initSmoothScrolling();
-  // initHeaderScroll();
   initAnimations();
   ensureHeroVideoAutoplay();
   initDropdowns();
@@ -12,8 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
   initCustomerStoriesImageFade();
   initDynamicBorderRadius();
 });
-
-// (Globe code removed by request)
 
 // Tab functionality for work process section
 function initTabs() {
@@ -88,26 +85,6 @@ function initSmoothScrolling() {
     });
   });
 }
-
-// Header scroll effect
-// function initHeaderScroll() {
-//     const header = document.querySelector('.header');
-//     let lastScrollTop = 0;
-
-//     window.addEventListener('scroll', () => {
-//         const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-//         if (scrollTop > 100) {
-//             header.style.background = 'rgba(255, 255, 255, 0.98)';
-//             header.style.boxShadow = '0 2px 20px rgba(0, 0, 0, 0.1)';
-//         } else {
-//             header.style.background = 'rgba(255, 255, 255, 0.95)';
-//             header.style.boxShadow = 'none';
-//         }
-
-//         lastScrollTop = scrollTop;
-//     });
-// }
 
 // Intersection Observer for animations
 function initAnimations() {
